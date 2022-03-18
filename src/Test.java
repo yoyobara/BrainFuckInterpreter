04c80f79ef5af46ceb7cf224756bfc068d5915e3
yoyobara <yotambarak6@gmail.com>
2022-03-18 17:17:52 +0200

    pc move

--- src/Test.java
+++ src/Test.java
@@ -1,5 +1,7 @@
 public class Test {
     public static void main(String[] args) {
-        System.out.println("".charAt(0));
+        System.out.println("hello");
+        System.out.println("hlad");
     }
 }
+g
